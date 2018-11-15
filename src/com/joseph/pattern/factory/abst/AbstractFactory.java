a237062abfec23342a4dfc8365fe830150a75260
GeekTJS <josephyean@gmail.com>
2018-11-15 21:32:28 +0800

    factory init

--- src/com/joseph/pattern/factory/abst/AbstractFactory.java
+++ src/com/joseph/pattern/factory/abst/AbstractFactory.java
@@ -6,6 +6,7 @@ import com.joseph.pattern.factory.Car;
  * 抽象工厂
  * 产品族概念
  * 理解：一家工厂生产他自己旗下的所有产品，奥迪工厂成生产不同的型号车子
+ * 缺点：不能添加产品
  * Created by joseph on 2018/11/15.
  */
 abstract class AbstractFactory {
