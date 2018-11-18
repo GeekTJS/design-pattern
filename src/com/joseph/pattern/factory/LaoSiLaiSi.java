9dc53ce8ca68305d22608041b0ad425b74f2d296
GeekTJS <josephyean@gmail.com>
2018-11-18 22:09:43 +0800

    useless commit

--- src/com/joseph/pattern/factory/LaoSiLaiSi.java
+++ src/com/joseph/pattern/factory/LaoSiLaiSi.java
@@ -1,9 +1,10 @@
 package com.joseph.pattern.factory;
 
+
 /**
  * Created by joseph on 2018/11/15.
  */
-public class LaoSiLaiSi implements Car{
+public class LaoSiLaiSi implements Car {
     @Override
     public String getName() {
         return "劳斯莱斯";
